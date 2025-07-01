a82ad3dd36fe07ddc7a8729c5f114a2d115c828e
Diana Lin <lindiana1206@gmail.com>
2025-07-01 12:34:41 -0400

    # This is a combination of 7 commits. # This is the 1st commit message:
    switch to ollama
    
    # This is the commit message #2:
    
    add full flask/ollama-based app
    
    # This is the commit message #3:
    
    improve markdown embeddings (remove drafts and duplicates; no longer embed by elements), use history-aware retriever
    
    # This is the commit message #4:
    
    fix: add chromadb
    
    # This is the commit message #5:
    
    separate scraping scripts
    
    # This is the commit message #6:
    
    split scrape scripts
    
    # This is the commit message #7:
    
    add chroma files

--- query.py
+++ query.py
@@ -1,53 +1,158 @@
-
-import argparse
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI 
+from langchain_chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
-
-from dotenv import load_dotenv
-import os
-import openai 
-
-load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from chromadb import PersistentClient
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+import json
+import uuid
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
 CHROMA_PATH = "./chroma"
+COLLECTION_NAME = "data"
+
+chat_instances = {}
+
+REPHRASE_PROMPT = '''
+Given the following conversation and follow-up, rephrase the follow-up question to be more specific and clear, while retaining the original intent.
+Conversation:
+{chat_history}
+
+Follow-up: {input}
+
+Standalone rephrased question:
+'''
 
 PROMPT_TEMPLATE = '''
-Answer the question based only on the following context: {context}
+Answer the question based on previous messages and the following context:
+---
+
+{context}
 
 ---
 Answer the question based on the above context: {question}
-
 '''
 
+def get_vector_store() -> Chroma:
+    persistent_client = PersistentClient(
+        path=CHROMA_PATH,
+    )
+
+    embeddings = OllamaEmbeddings(
+        base_url="http://localhost:11434",
+        model="qwen3",
+    )
+
+    return Chroma(
+        client=persistent_client,
+        collection_name=COLLECTION_NAME,
+        embedding_function=embeddings,
+    )
+
+def create_rag_chat() -> uuid.UUID:
+    """
+    Create a RAG chat instance using the Ollama model.
+    """
+    prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    
+    chat = ChatOllama(
+        base_url="http://localhost:11434",
+        model="qwen3",
+        temperature=0.1,  # Adjust temperature for more deterministic responses
+        prompt=prompt,
+    )
+
+    # Generate a unique ID for the chat instance
+    chat_id = str(uuid.uuid4())
+    chat_instances[chat_id] = {
+        "chat": chat,
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant for University of Virginia Research Computing (UVA RC). You will answer questions based on the context provided from the UVA RC YouTube channel and UVA RC Teaching Markdowns. Respond succinctly and accurately. If background knowledge combined with the context does not provide enough information, state that you do not know. If the user asks questions completely unrelated to the context or computing, politely inform them that you can only answer questions related to RC. If the user asks something related to RC, but the context does not provide enough information, state that you do not know."}
+        ],
+    }
+    
+    return chat_id
+
+def rag_query(chat_id: uuid.UUID, vector_store: Chroma, query: str) -> str:
+    """
+    Perform a RAG query on the Chroma vector store.
+    """
+    chat_instance = chat_instances[chat_id]
+    chat = chat_instance["chat"]
+
+    prompt = chat_instance["messages"]
+
+    # retriever_multi_query = MultiQueryRetriever.from_llm(
+    #     retriever=vector_store.as_retriever(),
+    #     llm=chat,
+    # )
+
+    # # Perform the retrieval
+    # docs = retriever_multi_query.invoke(query)
+
+    retriever = create_history_aware_retriever(
+        llm=chat,
+        retriever=vector_store.as_retriever(),
+        prompt=ChatPromptTemplate.from_template(REPHRASE_PROMPT),
+    )
+
+    # Perform the retrieval
+    docs = retriever.invoke({
+        "input": query,
+        "chat_history": prompt,
+    })
+
+    # Take the top 5 documents
+    docs = docs[:5]
+
+    context = "\n".join([result.page_content for result in docs]) if docs else "No relevant documents found."
+    print(f"Context: {context}")
+
+    prompt.append(
+        {"role": "user", "content": ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(
+            context=context,
+            question=query
+        )}
+    )
+
+    # disable thinking 
+    response = chat.invoke(prompt, think=False)
+
+    sources = {doc.metadata["source"]: doc.metadata for doc in docs}
+
+    sources_formatted = []
+    for source, metadata in sources.items():
+        match metadata["source_type"]:
+            case "youtube":
+                sources_formatted.append(f'[{metadata["title"]}]({metadata["webpage_url"]}) by [{metadata["author"]}](https://www.youtube.com/channel/{metadata["channel_id"]})')
+            case "markdown":
+                sources_formatted.append(f'[{metadata["source"]}]({metadata["source"]})')
+    
+    formatted_response = {
+        "response": response.content,
+        "sources": sources_formatted
+    }
+
+    prompt.append(
+        {"role": "assistant", "content": response.content}
+    )
+
+    return formatted_response
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query", type=str, help="Query text")
-    args = parser.parse_args()
-    query_text = args.query
+    vector_store = get_vector_store()
+    rag_chat = create_rag_chat()
 
-    embedding_func = OpenAIEmbeddings()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_func)
+    print(f"Number of documents in vector store: {len(vector_store.get()['ids'])}")
 
-    results=db.similarity_search_with_relevance_scores(query_text, k=3) # top 3 most similar
-    if len(results) == 0 or results[0][1] <0.7:
-        print(f"Bad results ")
-        return
-    context_text = "\n\n--\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+    while True:
+        query_text = input("Enter your query: ")
 
-    model= ChatOpenAI()
-    response_text = model.predict(prompt)
+        response = rag_query(rag_chat, vector_store, query_text)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
+        formatted_response = json.dumps(response, indent=2)
 
-    print(formatted_response)
+        print(formatted_response)
 
 
 if __name__ == "__main__":
